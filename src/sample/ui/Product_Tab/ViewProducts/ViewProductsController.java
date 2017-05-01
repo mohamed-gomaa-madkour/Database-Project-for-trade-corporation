365bb04d860b779363499422b52b8dc2817e8c5f
mostafaalsayed <moustafa.alsayed213@el-eng.menofia.edu.eg>
2017-05-01 20:12:28 +0200

    Add files via upload

--- src/sample/ui/Product_Tab/ViewProducts/ViewProductsController.java
+++ src/sample/ui/Product_Tab/ViewProducts/ViewProductsController.java
@@ -4,23 +4,25 @@ import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 
-public class ViewProductsController {
+    public class ViewProductsController {
 
-    @FXML
-    private JFXButton cancle;
+        @FXML
+        private JFXButton cancle;
 
-    @FXML
-    private TableColumn<?, ?> serial;
+        @FXML
+        private TableColumn<?, ?> serial;
 
-    @FXML
-    private TableColumn<?, ?> type;
+        @FXML
+        private TableColumn<?, ?> type;
 
-    @FXML
-    private TableColumn<?, ?> model;
+        @FXML
+        private TableColumn<?, ?> model;
 
-    @FXML
-    private TableColumn<?, ?> p_price;
+        @FXML
+        private TableColumn<?, ?> p_price;
 
-    @FXML
-    private TableColumn<?, ?> s_price;
-}
+        @FXML
+        private TableColumn<?, ?> s_price;
+
+    }
+
